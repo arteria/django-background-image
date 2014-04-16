ca758adda9d2807accb6cf1c969f421430bbf8bf
Philippe O. Wagner <wagner@arteria.ch>
2014-04-16 10:16:38 +0200

    Added setup.py for installation using PIP.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ django-background-image
 =======================
 
 Allow to set a background image from the admin site.
+
+Installation
+===
+
+pip install -e git+git://github.com/philippeowagner/django-background-image.git#egg=background_image
+
+
+Credits and kudos
+=================
+
+Single pattern taken from https://gist.github.com/senko/5028413. 
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, find_packages
+ 
+
+
+ 
+
+ 
+setup(
+    name="django-background-image",
+    version="0.0.1",
+    description="",
+    long_description="",
+    license='BSD',
+    platforms=['OS Independent'],
+    keywords='django, background image, style, css',
+    author='arteria GmbH, Philippe O. Wagner',
+    author_email='admin@arteria.ch',
+    url="https://github.com/philippeowagner/django-background-image",
+    packages=find_packages(),    
+)
